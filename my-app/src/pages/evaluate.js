0e3200a3e6f88feacdfb5a78f419b3d69d6d12fd
nunupy345345 <airinota@outlook.jp>
2023-05-29 21:48:21 +0900

    <evaluateValue> お金を表示する仕組み(途中)

--- my-app/src/pages/evaluate.js
+++ my-app/src/pages/evaluate.js
@@ -1,4 +1,5 @@
 import {NavLink} from "react-router-dom";
+import React, { useState } from 'react' 
 
 export const Evaluate = () => {
   return (
@@ -8,6 +9,40 @@ export const Evaluate = () => {
       <br/>
       <NavLink to="/">ホームに戻る</NavLink>
       <h1>評価</h1>
+    </div>
+  );
+};
+
+// ウニの評価を行う関数
+const evaluateUni = (uniData) => {
+  //unidataを定義する
+  const { size, beauty, agility, taste} = uniData;
+  // 金額を定義する(金額は適当)
+  const price = size * 1000 + beauty * 500 + agility * 100 + taste * 5000;
+  //値として金額を返す（あとで金額を表示させたい）
+  return price;
+};
+
+//なんもないときのウニのデータ
+const NomalUniData = () => {
+  const [uniData, setUniData] = useState({
+    size: 0,
+    beauty: 0,
+    agility: 0, //素早さ
+    taste: 0,
+  });
+}
+
+//ウニのデータを更新（後で考える）
+
+
+//ウニ単体のお金を表示する
+export const DisplayPrice = () => {
+  return(
+    <div className="evaluate">
+      <h1>
+        {evaluateUni}円Get!
+      </h1>
     </div>
   );
 };
