d6ad6344ece7c47eae3880f74121986d0ac05163
nunupy345345 <airinota@outlook.jp>
2023-06-03 21:53:13 +0900

    <commit> これを使ってください

--- my-app/src/pages/start.js
+++ my-app/src/pages/start.js
@@ -1,15 +1,17 @@
 import {NavLink} from "react-router-dom";
 
 export const Start = () => {
+  //clickしたときにtrainのページにいくようにする
   const handleClick = () => {
     window.location.href = "/train";
   }
+  //表示されるもの
   return (
     <div className="start">
       <img src={`${process.env.PUBLIC_URL}/start/dummy.png`} alt="start"/>
      
       <h1>開始ページ</h1>
-      <img src={`${process.env.PUBLIC_URL}/ozo-uni.PNG`} onClick={handleClick} id="hai" />
+      <img src={`${process.env.PUBLIC_URL}/ozo-uni.PNG`} onClick={handleClick} id="hai" width="100"/>
     </div>
   );
 };
