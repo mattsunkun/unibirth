a55224f53f4b82ca1a7b675d2e5d864fff86fbd4
hrk-mon-ddd23 <best5_song2_ever3@docomo.ne.jp>
2023-06-05 11:24:24 +0900

    <background> 背景画像を設定しました

--- my-app/src/pages/train.js
+++ my-app/src/pages/train.js
@@ -1,5 +1,9 @@
 import {NavLink,useLocation} from "react-router-dom";
 
+import React, { useState } from 'react';
+
+
+
 export const Train = () => {
   //ページを変えても値を受け渡すやつ
   const search = useLocation().search;
@@ -9,7 +13,21 @@ export const Train = () => {
   //初期のお金設定
   const firstPrice = 1000
 
-  //仮のウニのパラメーター
+  //アイテムボタンを押したらアイテムが表示される
+  
+  
+  
+
+
+  //購入するアイテムの金額と種類
+  
+
+  //購入する物を選択してチェックがつくようにする
+  
+
+  
+
+  //仮のウニのパラメーター　
   const agilityNum = 3
   const tasteNum = 3
   const sizeNum = 3
@@ -25,21 +43,85 @@ export const Train = () => {
 
 
   //持ってるお金を定義
+  const [agility, setAgility] = useState(0);
+  const [beauty, setBeauty] = useState(0);
+  const [size, setSize] = useState(0);
+  const [taste, setTaste] = useState(0);
+
+  // ウニの評価金額を計算する関数
+  const evaluateUni = () => {
+    const totalEvaluation = beauty + agility + size + taste;
+    return totalEvaluation;
+  };
+
+  const handleBeautyChange = (event) => {
+    setBeauty(Number(event.target.value));
+  };
+
+  const handleAgilityChange = (event) => {
+    setAgility(Number(event.target.value));
+  };
+
+  const handleSizeChange = (event) => {
+    setSize(Number(event.target.value));
+  };
+
+  const handleTasteChange = (event) => {
+    setTaste(Number(event.target.value));
+  };
+
+  
+  
+
   let haveMoney = () => {
-      haveMoney = Number(query2.get('money') + 1000)
+      haveMoney = Number(Number(query2.get('money')) + 1000)
       return haveMoney
   }
   
   
+ //背景画像指定
+ const backImagePath = `${process.env.PUBLIC_URL}/buying.PNG`
 
  //表示するところ
   return (
-      <div className="train">
-      <img src={`${process.env.PUBLIC_URL}/train/dummy.png`} alt="train"/>
+    <div className="train"
+     style = {
+      { backgroundImage: `url(${backImagePath})` }
+     }>
+   
      
-      <h1>育成</h1>
+
+    
+
+      <h1> ウニを育成しよう</h1>
       <div>持っているお金:  {haveMoney()} 円</div>
       <img src={`${process.env.PUBLIC_URL}/ozo-uni.PNG`} onClick={handleClick} id="hai" width="100"/>
+      <h2>ウニ評価</h2>
+      <label>
+        美しさ：
+        <input type="number" value={beauty} onChange={handleBeautyChange} />
+      </label>
+      <br />
+      <label>
+        素早さ：
+        <input type="number" value={agility} onChange={handleAgilityChange} />
+      </label>
+      <br />
+      <label>
+        大きさ：
+        <input type="number" value={size} onChange={handleSizeChange} />
+      </label>
+      <br />
+      <label>
+        美味しさ：
+        <input type="number" value={taste} onChange={handleTasteChange} />
+      </label>
+      <br />
+      <p>ウニの評価金額は {evaluateUni()} 円です。</p>
+      
+
+
+
       
     </div>
   )
