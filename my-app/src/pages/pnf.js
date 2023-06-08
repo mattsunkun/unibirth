5eb62ed9ce9142a157aa64737cf0435eb18040fb
nunupy345345 <airinota@outlook.jp>
2023-06-08 19:46:07 +0900

    <404,instruction> instructionと404ページを全画面表示できるようにしました。

--- my-app/src/pages/pnf.js
+++ my-app/src/pages/pnf.js
@@ -1,11 +1,28 @@
-import {NavLink} from "react-router-dom";
+import {NavLink,useLocation} from "react-router-dom";
+import useSound from 'use-sound';
 
 export const Pnf = () => {
+  //クリック音設定
+  const [ClickSound] = useSound(`${process.env.PUBLIC_URL}/clickSound.mp3`)
+  
+  //page移動
+  const handleClick1 = () => {
+    window.location.href = "/start";
+  }
+
+  const backImagePath = `${process.env.PUBLIC_URL}/404-uni.jpg`
   return (
-    <div className="pnf">
-      <img src={`${process.env.PUBLIC_URL}/other/pnf.png`} alt="pikmin"/>
-      <h1>ページが見つかりません</h1>
-      <NavLink to="/">ホームに戻る</NavLink>
+    <div className="pnf"
+    style={
+      { backgroundImage: `url(${backImagePath})`,
+        backgroundRepeat: 'no-repeat',
+        height: '1020px',
+        width: '1440px'
+        }
+    }>
+
+     
+      <img src={`${process.env.PUBLIC_URL}/start.png`} alt="" width="100" onClick={() => {ClickSound(); handleClick1()}} id="hai" /> 
 
     </div>
   );
